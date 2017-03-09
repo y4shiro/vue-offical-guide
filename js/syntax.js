0ade1ab24f4c1b9e273d8a250f80dd8e54d72c35
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-03-09 19:14:27 +0900

    end syntax.html

--- js/syntax.js
+++ js/syntax.js
@@ -11,3 +11,44 @@ let vm2 = new Vue({
     msg: '変わらないよ！'
   }
 })
+
+let vm3 = new Vue({
+  el: '#raw',
+  data: {
+    msg: '<h1>h1</h1>'
+  }
+})
+
+let vm4 = new Vue({
+  el: '#bind',
+  data: {
+    t: 'binded'
+  }
+})
+
+let vm5 = new Vue({
+  el: '#bool',
+  data: {
+    t: true,
+    f: false
+  }
+})
+
+let vm6 = new Vue({
+  el: '#ref',
+  data: {
+    url: 'google.com'
+  }
+})
+
+let vm7 = new Vue({
+  el: '#click',
+  data: {
+    msg: 'まだクリックしてない'
+  },
+  methods: {
+    isClick: function () {
+      this.msg = 'くりっくした'
+    }
+  }
+})
