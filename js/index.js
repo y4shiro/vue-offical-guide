bd345fe5844251dd3f9f40a4263246a93f028bbe
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2017-03-09 15:46:23 +0900

    app7

--- js/index.js
+++ js/index.js
@@ -48,3 +48,19 @@ var app6 = new Vue({
     message: 'Hello Vue.js!'
   }
 })
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { text: 'Vegetables' },
+      { text: 'Cheese' },
+      { text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
